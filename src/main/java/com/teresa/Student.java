680a75183b6cb6d8bfaec8855920fcb637667cc0
courtney404570281 <404570281@gapp.fju.edu.tw>
2019-02-25 21:11:40 +0800

    Initial commit

---
+++ src/main/java/com/teresa/Student.java
@@ -0,0 +1,7 @@
+package com.teresa;
+
+public class Student {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/kotlin/Student.kt
@@ -0,0 +1,7 @@
+class Student (var english: Int, var math: Int) {
+
+}
+
+fun main () {
+    val stu = Student (77, 88);
+}
