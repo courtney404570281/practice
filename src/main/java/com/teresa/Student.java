e1935c090b435ae1145b7976b02fe6fa58bc1680
courtney404570281 <404570281@gapp.fju.edu.tw>
2019-02-25 21:22:29 +0800

    Constructure

--- src/main/java/com/teresa/Student.java
+++ src/main/java/com/teresa/Student.java
@@ -1,7 +1,15 @@
 package com.teresa;
 
 public class Student {
-    public static void main(String[] args) {
+    int english;
+    int math;
 
+    public Student(int english, int math){
+        this.english = english;
+        this.math = math;
+    }
+
+    public static void main(String[] args) {
+        Student stu = new Student(77, 88);
     }
 }
--- src/main/kotlin/Student.kt
+++ src/main/kotlin/Student.kt
@@ -1,7 +1,9 @@
-class Student (var english: Int, var math: Int) {
+class Student (val english: Int, var math: Int) {
 
 }
 
 fun main () {
     val stu = Student (77, 88);
+    //stu.english = 99;  //wrong
+    stu.math = 90; //right!!
 }
